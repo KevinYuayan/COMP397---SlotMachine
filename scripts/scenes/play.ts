398012c02c99e3b8ece7638890a334d6f8c55699
Kevin Yuayan <yuayankevin@gmail.com>
2018-10-04 13:40:55 -0300

    first commit

---
+++ scripts/scenes/play.ts
@@ -0,0 +1,32 @@
+module scenes{
+    export class Play extends objects.Scene{
+        // Private instance variables
+        // private _slotMachine:objects.SlotMachine;
+        // public variables
+
+        // constructor
+        constructor() {
+            super();
+        }
+        // private methods
+
+        // public methods
+        
+        public Main(): void {
+            throw new Error("Method not implemented.");
+        }        public Start(): void {
+            throw new Error("Method not implemented.");
+        }
+        public Update(): void {
+            throw new Error("Method not implemented.");
+        }
+        public Reset(): void {
+            throw new Error("Method not implemented.");
+        }
+        public Destroy(): void {
+            throw new Error("Method not implemented.");
+        }
+
+
+    }
+}
